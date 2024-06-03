0ea71f99b21dbc3ea2818edd704054a5c89d3afd
HUIJAEKO <xrhgmlwox@naver.com>
2024-06-03 22:11:07 +0900

    exam

--- GoodByeFinalExam/src/main/java/org/example/goodbyefinalexam/HelloApplication.java
+++ GoodByeFinalExam/src/main/java/org/example/goodbyefinalexam/HelloApplication.java
@@ -1,30 +1,60 @@
 package org.example.goodbyefinalexam;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage primaryStage){
-        HBox h = new HBox(new Button("ok"), new Button("cancel"));
-        h.setAlignment(Pos.CENTER);
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        ImageView us = new ImageView(new Image("image/us.gif"));
+        Label lb1 = new Label("US\n50 States", us);
+        lb1.setStyle("-fx-border-color: green; -fx-border-width: 2");
+        lb1.setContentDisplay(ContentDisplay.BOTTOM);
+        lb1.setTextFill(Color.RED);
+
+        Label lb2 = new Label("Circle", new Circle(50, 50, 25));
+        lb2.setContentDisplay(ContentDisplay.TOP);
+        lb2.setTextFill(Color.ORANGE);
+
+        Label lb3 = new Label("Rectangle", new Rectangle(10, 10, 50, 25));
+        lb3.setContentDisplay(ContentDisplay.RIGHT);
+
+        Label lb4 = new Label("Ellipse", new Ellipse(50, 50, 50, 25));
+        lb4.setContentDisplay(ContentDisplay.LEFT);
+
+        Ellipse ellipse = new Ellipse(50, 50, 50, 25);
+        ellipse.setStroke(Color.GREEN);
+        ellipse.setFill(Color.WHITE);
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(ellipse, new Label("JavaFX"));
+        Label lb5 = new Label("A pane inside a label", stackPane);
+        lb5.setContentDisplay(ContentDisplay.BOTTOM);
+
+        HBox pane = new HBox(20);
+        pane.getChildren().addAll(lb1, lb2, lb3, lb4, lb5);
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane, 450, 150);
+        primaryStage.setTitle("LabelWithGraphic"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
     }
-
-
+    
     public static void main(String[] args) {
         launch(args);
     }
 }
 
-class OKHandler implements EventHandler<ActionEvent>{
-    @Override
-    public void handle(ActionEvent e){
-        System.out.println("ok");
-    }
-}
+
+
+
+
