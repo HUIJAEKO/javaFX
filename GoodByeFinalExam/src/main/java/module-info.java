cd0012bf34d8497cd5763d168ed5c1a91ab26b1e
HUIJAEKO <143581510+HUIJAEKO@users.noreply.github.com>
2024-06-07 17:33:50 +0900

    Delete GoodByeFinalExam directory

--- GoodByeFinalExam/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.goodbyefinalexam {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.goodbyefinalexam to javafx.fxml;
-    exports org.example.goodbyefinalexam;
-}
